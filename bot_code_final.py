1c5ca6eab6f189756c5c2e3ee104dd8b876738f3
meteg171212-cloud <meteg171212@icloud.com>
2026-05-26 19:02:51 +1000

    Add files via upload

---
+++ bot_code_final.py
@@ -0,0 +1,40 @@
+import discord
+import random
+from discord.ext import commands
+import os
+import requests
+import random
+from model import get_class
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+name = ""
+
+
+
+
+bot = commands.Bot(command_prefix='@', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+
+@bot.command()
+async def check_image(ctx):
+    if ctx.message.attachments:
+        for attachment in ctx.message.attachments:
+            file_name = attachment.filename
+            file_url = attachment.url
+            await attachment.save(f"./{attachment.filename}")
+            await ctx.send(get_class(model_path="./keras_model.h5", labels_path="labels.txt", image_path=f"./{attachment.filename}"))
+    else:
+        await ctx.send("You forgot to upload the image :(")
+
+
+bot.run("MTUwMTEzOTI2MzE4ODg5Mzg0Nw.GWA7R6.aCz0aBfeZ0KJYB2hSO3zKhxWP233JJoEvMQ9Xc")
